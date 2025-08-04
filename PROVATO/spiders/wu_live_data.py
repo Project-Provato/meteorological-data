03df670c75be9ce15f31edcc3940120d19284486
Manos <50167081+Manos1Dev@users.noreply.github.com>
2025-08-04 16:38:02 +0300

    [wu] add nomos field

--- PROVATO/spiders/wu_live_data.py
+++ PROVATO/spiders/wu_live_data.py
@@ -41,6 +41,7 @@ class Meteo_Live_Data(scrapy.Spider):
         timedata = self.get_day_and_hour(response)
         crawled = dt.now()
         city = response.meta['city']
+        nomos = response.meta['nomos']
 
         all_measurements = {}
 
