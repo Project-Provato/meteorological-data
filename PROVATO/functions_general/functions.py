41f0d3d66f12241ab7b11a8d7b455d823893172c
Manos <50167081+Manos1Dev@users.noreply.github.com>
2025-08-07 20:24:05 +0300

    remove general functions

--- PROVATO/functions_general/functions.py
+++
@@ -1,7 +0,0 @@
-def convert_day(day):  # input: [DD/MM/YY HH:MM] | output: [DD/MM]
-    details = day.split(' ')[0].split('/')
-
-    return details[0] + '/' + details[1]
-
-def convert_hour(hour):  # input: [DD/MM/YY HH:MM] | output: [HH:MM]
-    return hour.split(' ')[1]
